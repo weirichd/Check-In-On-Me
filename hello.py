ccd481e116c1b5dc8d99d778eb38ed1dd2e6b682
David E. Weirich <weirich.david@gmail.com>
2020-03-29 21:12:19 -0400

    2nd tutorial

--- hello.py
+++ hello.py
@@ -1,18 +1,36 @@
 from flask import Flask
+from flask import render_template, url_for
+
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'David Weirich',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'April 1, 2020',
+    },
+    {
+        'author': 'Emily Huang',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 2, 2020',
+    },
+
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
