dddcee76b0e4198ef86a677f354fe8c60596af66
David E. Weirich <weirich.david@gmail.com>
2020-04-02 23:37:15 -0400

    Submit button

--- check_in.py
+++ check_in.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 import pandas as pd
 
@@ -8,15 +9,20 @@ app = Flask(__name__)
 
 df = pd.DataFrame(
     {
-        "Name": ["David", "Edward", "Weirich"],
+        "Name": ["David Weirich", "Edward", "Weirich"],
         "Last Contacted": ["2020-01-01", "2020-01-02", "2020-02-19"],
     }
 )
+
 df["Last Contacted"] = pd.to_datetime(df["Last Contacted"])
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    global df
+
+    if request.method == 'POST':
+        print('Clicked on', request.form.get('update'))
 
     return render_template("check_in.html", df=df)
 
