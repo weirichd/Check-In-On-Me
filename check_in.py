04d647d6bc7ac66261c4c93805f18b91d367c2bd
David E. Weirich <weirich.david@gmail.com>
2020-04-03 20:48:37 -0400

    Reload button

--- check_in.py
+++ check_in.py
@@ -6,17 +6,20 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# The CSV needs to have columns Name, Last Contacted, Cadence
 df = pd.read_csv('people.csv')
 
 
 def update_df():
     global df
 
-    df = df.sort_values('Last Contacted')
     df["Last Contacted"] = pd.to_datetime(df["Last Contacted"])
     df["Days Since"] = (pd.Timestamp.now() - df['Last Contacted']).apply(lambda x: x.days)
+    df['Days Remaining'] = df['Cadence'] - df['Days Since']
 
-    df.drop('Days Since', axis=1).to_csv('people.csv', index=False)
+    df = df.sort_values(['Days Remaining', 'Last Contacted'])
+
+    df.drop(['Days Since', 'Days Remaining'], axis=1).to_csv('people.csv', index=False)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -24,11 +27,13 @@ def home():
     global df
 
     if request.method == 'POST':
-        idx = int(request.form.get('update'))
-
-        print('Clicked on', df.loc[idx, 'Name'])
-
-        df.loc[idx, 'Last Contacted'] = pd.Timestamp.now()
+        if request.form.get('update') is not None:
+            idx = int(request.form.get('update'))
+            print('Clicked on', df.loc[idx, 'Name'])
+            df.loc[idx, 'Last Contacted'] = pd.Timestamp.now()
+        elif request.form.get('reload') is not None:
+            print('Reloading Page')
+            df = pd.read_csv('people.csv')
 
         update_df()
 
